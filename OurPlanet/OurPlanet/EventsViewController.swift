76f6a5aafb3d1e77c6e837ea8f7db306ab7ee243
Chuy <chuymaster3@hotmail.com>
2018-09-22 18:04:00 +0900

    Pass events to events view controller

--- OurPlanet/OurPlanet/EventsViewController.swift
+++ OurPlanet/OurPlanet/EventsViewController.swift
@@ -24,29 +24,43 @@ import UIKit
 import RxSwift
 
 class EventsViewController : UIViewController, UITableViewDataSource {
-
-  @IBOutlet var tableView: UITableView!
-  @IBOutlet var slider: UISlider!
-  @IBOutlet var daysLabel: UILabel!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    tableView.rowHeight = UITableViewAutomaticDimension
-    tableView.estimatedRowHeight = 60
-  }
-
-  @IBAction func sliderAction(slider: UISlider) {
-  }
-  
-  // MARK: UITableViewDataSource
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
-    return cell
-  }
-  
+    
+    let events = Variable<[EOEvent]>([])
+    let disposeBag = DisposeBag()
+    
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var daysLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 60
+        
+        events.asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    @IBAction func sliderAction(slider: UISlider) {
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
+        let event = events.value[indexPath.row]
+        cell.configure(event: event)
+        return cell
+    }
+    
 }
+
